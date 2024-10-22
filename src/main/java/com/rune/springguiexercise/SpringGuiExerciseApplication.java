4fd122a1b531e481a1132303820eb6f8da8d7a39
Rune Hansen <rune@roeddik.net>
2024-10-22 14:44:53 +0200

    Initial commit

---
+++ src/main/java/com/rune/springguiexercise/SpringGuiExerciseApplication.java
@@ -0,0 +1,13 @@
+package com.rune.springguiexercise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringGuiExerciseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringGuiExerciseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/rune/springguiexercise/SpringGuiExerciseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rune.springguiexercise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringGuiExerciseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
